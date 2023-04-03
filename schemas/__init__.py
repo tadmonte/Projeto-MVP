88e682e24af4f40f09a7ce3cea426ee10f90e348
Tadeu <tadeu.fco@gmail.com>
2023-04-03 12:36:02 -0300

    Tudo rodando já com validade e unidade. Banco OK

---
+++ schemas/__init__.py
@@ -0,0 +1,5 @@
+
+from schemas.produto import ProdutoSchema, ProdutoBuscaSchema, ProdutoViewSchema, \
+                            ListagemProdutosSchema, ProdutoDelSchema, apresenta_produtos, \
+                            apresenta_produto, apresenta_produtos
+from schemas.error import ErrorSchema
